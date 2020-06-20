ca8643c1a2b635d3aee1ae1eb388bf59e02d2700
Bobby Sporman <bobby.sporman@yahoo.com>
2020-06-20 15:06:30 -0400

    Final updates

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,42 +1,54 @@
-/*Here I am setting the main variables that the function will take into place. Line 4 will set the password length to be false, 
-because it needs to have a certain amount of characters. Line 5 is responsible for the interacctive generate button. Lines 7-21 are the characters responsible for sending the password, 
-which the user will be able to choose. Line 22 is responsible for displaying the password in the text box.*/
-var passwordLength = false;
+/* The below variable is responsible for the interacctive generate button. When clicked it will begin the prompts and confrims below. */
 var generateBtn = document.querySelector("#generate");
-var password = "";
-var charCollection =
-  "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+0123456789";
-var charCollectionTwo =
-  "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var charCollectionThree = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0!@#$%^&*_-+123456789";
-var charCollectionFour = "abcdefghijklmnopqrstuvwxxyz!@#$%^&*_-+0123456789";
-var charCollectionFive =
-  "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+";
-var charCollectionSix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-var charCollectionSeven = "abcdefghijklmnopqrstuvwxxyz0123456789";
-var charCollectionEight =
-  "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charCollectionNine = "!@#$%^&*_-+0123456789";
-var charCollectionTen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+";
-var charCollectionEleven = "abcdefghijklmnopqrstuvwxxyz!@#$%^&*_-+";
-var charCollectionTwelve = "abcdefghijklmnopqrstuvwxxyz";
-var charCollectionThirteen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charCollectionFourteen = "!@#$%^&*_-+";
-var charCollectionFifteen = "0123456789";
-var passwordText = document.querySelector("#password");
 
-/* Here we are setting the function (line 29) which is going to essentially take the user input, with all the conditions, and generate a password of their choosing. 
-The function starts with a while statement (line 30) that will prompt the user to choose a password length between 8 and 128 characters. Once the user inputs the correct amount 
-of characters they will be asked to confirm if they want to use special characters, upper and lower case letters as well as numbers(lines 33-38). Once the character values
-are confirmed, the function will utilize the IF statements below(lines 40-118), one for each option the user can potentially use, and spit back out the password
-which is defined on line 117 with the return password code.*/
+/* Below is the function will write the passowrd to the given text box located on the webpage. */
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+/* The below is responsible for displaying the password in the text box and responding to the generate button on the main webpage. */
+generateBtn.addEventListener("click", writePassword);
+passwordText.value = "";
+passwordText.innerHTML = "";
+
+/* Below is the function which will prompt a series of questions and depending what the user preference is, will send a result. */
 function generatePassword() {
+  /* The password length to be false, because it needs to have a certain amount of characters. */
+  var passwordLength = false;
+  /* Below I am setting the main variables that the function will take into place. Most of the variables define the characters responsible for sending the password.*/
+  var charCollection =
+    "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+0123456789";
+  var charCollectionTwo =
+    "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charCollectionThree = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0!@#$%^&*_-+123456789";
+  var charCollectionFour = "abcdefghijklmnopqrstuvwxxyz!@#$%^&*_-+0123456789";
+  var charCollectionFive =
+    "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+";
+  var charCollectionSix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var charCollectionSeven = "abcdefghijklmnopqrstuvwxxyz0123456789";
+  var charCollectionEight =
+    "abcdefghijklmnopqrstuvwxxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charCollectionNine = "!@#$%^&*_-+0123456789";
+  var charCollectionTen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_-+";
+  var charCollectionEleven = "abcdefghijklmnopqrstuvwxxyz!@#$%^&*_-+";
+  var charCollectionTwelve = "abcdefghijklmnopqrstuvwxxyz";
+  var charCollectionThirteen = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charCollectionFourteen = "!@#$%^&*_-+";
+  var charCollectionFifteen = "0123456789";
+  var password = "";
+
+/* The while statement below outlines what the length of the password is needed to be.*/
   while (passwordLength < 8 && passwordLength > 128 && passwordLength === "");
   {
+    /* This prompt will ask the user to choose a length between 8 and 128 characters. */
     passwordLength = prompt(
       "Please choose a valid length: Choose a length between 8 and 128"
     );
   }
+
+
+  /* Below the conditionals are taking place. Whatever characters the user would like to use for his/her password, the code below will take the user input into consideration and then return their password, based on what was confirmed.*/
   if (passwordLength < 128 && passwordLength > 8);
   {
     var chooseSpecialCharacters = confirm("Do you want special characters?");
@@ -44,6 +56,7 @@ function generatePassword() {
     var chooseUppercase = confirm("Do you want uppercase letters?");
     var chooseNumbers = confirm("Do you want numbers?");
   }
+
 
   if (
     chooseSpecialCharacters === true &&
@@ -231,37 +244,6 @@ function generatePassword() {
         Math.floor(Math.random() * charCollectionFifteen.length)
         ];
   }
-  passwordText.value = "";
-  passwordText.innerHTML = "";
-
+/* This will return the password based off the criteria chosen. */
   return password;
 }
-
-/*Below is the code that essentially starts the motion of generating the password. Line 132 is the code that makes the interactive button on the webpage
-generate the prompts and confirms.       */
-function writePassword() {
-  var password = generatePassword();
-  passwordText.value = password;
-}
-generateBtn.addEventListener("click", writePassword);
-
-// var passwordText = document.querySelector("#password");
-// generateBtn.addEventListener('click', generatePassword);
-
-//generatePassword();
-// if user wants another password, set the .value of document.querySelector("#password") === '' and then generatePassword()
-//else alert("Here is your password") then return password.
-
-// var newPass = confirm("Do you want another password?");
-// if(newPass === true);
-// return function generatePassword();
-
-// writePassword();
-
-// function reset(){
-//   passwordText.value = ""
-// }
-
-// //window.addEventListener("click", function(){
-//   document.getElementById("#password").innerHTML
-// });
